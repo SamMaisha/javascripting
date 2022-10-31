91092b4a960fac272b6045288bdeb4be22dc0337
Sam Maisha <sam.maisha99@gmail.com>
2022-10-31 19:44:34 +0000

    Initial commit with my work for javascripting exercice

---
+++ for-loop.js
@@ -0,0 +1,5 @@
+total = 0;
+limit = 10;
+for (i = 0; i < 10; total += i) {
+  console.log(total);
+}
---
+++ looping-through-arrays.js
@@ -0,0 +1,4 @@
+let pets = ['cat', 'dog', 'rat']
+for (i = 0, i < 2, i++) {
+  console.log(pets[i] + 's')
+}
